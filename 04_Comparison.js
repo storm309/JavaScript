08991469ca08bbb2d8f44dea2664d6cced643c6e
Shivam Pandey <shivamkumarp447@gmail.com>
2025-02-05 20:38:50 +0530

    add comparisons

---
+++ 04_Comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 3);
+// console.log(2 < 3);
+// console.log(2 >= 3);
+// console.log(2 <= 3);
+// console.log(2 == 3);
+// console.log(2 != 3);
+
+// console.log("2" > 3);
+// console.log("2" < 3);
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined <= 0); // false
+
+// === => strict equality
+// == => loose equality
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false
