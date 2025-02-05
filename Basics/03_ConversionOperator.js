c5e8555ef0abf7b20dfe7208a7d82d86437bd1a6
Shivam Pandey <shivamkumarp447@gmail.com>
2025-02-05 19:59:27 +0530

    add Conversion Operation

---
+++ Basics/03_ConversionOperator.js
@@ -0,0 +1,34 @@
+// let score = "33abc"
+
+// let score = null
+
+// let score = undefined
+
+// let score = Boolean
+
+// let score = "Shivam"
+
+// console.log(typeof score) // number
+// console.log(typeof(score)) // number
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // number
+// console.log(valueInNumber) // NaN
+
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// Boolean => NaN
+// "Shivam" => NaN
+// "33" => 33
+// true => 1 false => 0
+// let isLoggedIn =  "Shivam" // true
+let isLoggedIn =  0 // false
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) 
+
+let someNumber = 33
+
+let strinNumber = String(someNumber)
+console.log(strinNumber);
+console.log(typeof strinNumber); // string
