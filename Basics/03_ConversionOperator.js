77f3233a2a9cab76546fbecd5dbdda553c8140b2
Shivam Pandey <shivamkumarp447@gmail.com>
2025-02-05 20:26:22 +0530

    add Operations

--- Basics/03_ConversionOperator.js
+++ Basics/03_ConversionOperator.js
@@ -23,12 +23,50 @@
 // "33" => 33
 // true => 1 false => 0
 // let isLoggedIn =  "Shivam" // true
-let isLoggedIn =  0 // false
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn) 
+// let isLoggedIn =  0 // false
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn) 
 
-let someNumber = 33
+// let someNumber = 33
 
-let strinNumber = String(someNumber)
-console.log(strinNumber);
-console.log(typeof strinNumber); // string
+// let strinNumber = String(someNumber)
+// console.log(strinNumber);
+// console.log(typeof strinNumber); // string
+
+// ********** Operations **********
+
+let value = 3
+let negValue = -value
+// console.log(negValue) // -3
+
+// console.log(2 + 3) // 5
+// console.log(2 - 3) // -1
+// console.log(2 * 3) // 6
+// console.log(2 / 3) // 0.6666666666666666
+// console.log(2 % 3) // 2
+// console.log(2 ** 3) // 8
+
+let str= "hello"
+let sr2 = "shivam"
+let str3 = str + sr2
+// console.log(str3)
+
+// console.log("1" + 2) // 12
+// console.log(1 + "2") // 12
+// console.log("1" + 2 + 2) // 122
+// console.log(1 + 2 + "2") // 32
+
+// console.log((3 + 4) * 5 % 3); // 2
+
+// console.log(+true) // 1
+// console.log(+"") // 0
+
+// let num1, mnum2, num3
+// num1 = num2 = num3 = 2 + 2
+// console.log(num1, num2, num3) // 4 4 4
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); // 101
+gameCounter--;
+console.log(gameCounter); // 100
