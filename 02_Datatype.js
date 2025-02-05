38a3a9e39ca4a6ff9fc256b253311280222a1f21
Shivam Pandey <shivamkumarp447@gmail.com>
2025-02-05 20:24:57 +0530

    add operations

---
+++ 02_Datatype.js
@@ -0,0 +1,32 @@
+"Use Strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodeJs, not browser
+
+console.log(3 + 
+    3);  // code readability should be high
+console.log("Shivam");
+
+let name = "Shivam"
+let age = 20
+let isLoggedIn = true
+let state = null
+
+// Priitive Data Types
+
+// number => 2*53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof "Shivam")
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined bcz of type
+
+
+
